41ed56c19e231178004de0dec6e5fda96795d934
Kuba Sadowy <kubasadowy2@gmail.com>
2023-02-01 00:25:02 +0100

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { TableHead } from "./Components/TableHead";
+import { useState } from "react";
+import { toDoData } from "./toDoData";
+import { ToDoTask } from "./Components/ToDoTask";
+import { CreateNewTask } from "./Components/CreateNewTask";
 
 function App() {
+  const [toDo, setToDo] = useState(toDoData);
+
   return (
     <div className="App">
       <h1>To Do App</h1>
       <TableHead />
-    </div>
+      {
+        toDo.map((toDo, index) => (
+          <ToDoTask
+            index={index}
+            toDo={toDo} />
+        ))
+      }
 
+      <div>
+        <CreateNewTask />
+      </div>
+    </div>
   );
 }
-
 export default App;
