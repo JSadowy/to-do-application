9d7141fd3f1a0933a8139546db4d561b823dcd82
Kuba Sadowy <kubasadowy2@gmail.com>
2023-02-01 13:29:50 +0100

    Add new Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import { TableHead } from "./Components/TableHead";
-import { useState } from "react";
-import { toDoData } from "./toDoData";
-import { ToDoTask } from "./Components/ToDoTask";
-import { CreateNewTask } from "./Components/CreateNewTask";
 
 function App() {
-  const [toDo, setToDo] = useState(toDoData);
-
   return (
     <div className="App">
       <h1>To Do App</h1>
       <TableHead />
-      {
-        toDo.map((toDo, index) => (
-          <ToDoTask
-            index={index}
-            toDo={toDo} />
-        ))
-      }
-
-      <div>
-        <CreateNewTask />
-      </div>
     </div>
+
   );
 }
+
 export default App;
